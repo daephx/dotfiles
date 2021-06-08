7b3c1c81eadf7e1a5bdcd4a0921f0a0449b3e198
daephx <36192863+daephx@users.noreply.github.com>
2021-06-08 00:51:30 -0700

    refactor to use dotbot

--- install-profile.sh
+++ install-profile.sh
@@ -14,14 +14,24 @@ DOTBOT_BIN="bin/dotbot"
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+
 cd "${BASE_DIR}"
 git submodule update --init --recursive
 
-for config in ${@}; do
+
+while IFS= read -r config; do
+    CONFIGS+=" ${config}"
+done < "${META_DIR}/${PROFILES_DIR}/$1"
+
+shift
+
+
+for config in ${CONFIGS} ${@}; do
+    echo -e "\nConfigure $config"
     # create temporary file
     configFile="$(mktemp)"
     suffix="-sudo"
-    echo -e "$(<"${BASE_DIR}/${META_DIR}/${BASE_CONFIG}${CONFIG_SUFFIX}")\n$(<"${BASE_DIR}/${META_DIR}/${CONFIG_DIR}/${config%"$suffix"}${CONFIG_SUFFIX}")" >"$configFile"
+    echo -e "$(<"${BASE_DIR}/${META_DIR}/${BASE_CONFIG}${CONFIG_SUFFIX}")\n$(<"${BASE_DIR}/${META_DIR}/${CONFIG_DIR}/${config%"$suffix"}${CONFIG_SUFFIX}")" > "$configFile"
 
     cmd=("${BASE_DIR}/${META_DIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASE_DIR}" -c "$configFile")
 
