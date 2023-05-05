5df826cffcb8258c26215ec96438742a676deec2
daephx <daephxdev@gmail.com>
2023-05-05 12:11:02 -0700

    fix: rename profile.d scripts to have wider priority values

--- config/profile.d/10-xdg-base-dirs.sh
+++ config/profile.d/10-xdg-base-dirs.sh
@@ -1,9 +1,13 @@
+# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+
 # XDG base directory specifications
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_LOCAL_HOME="$HOME/.local"
 export XDG_STATE_HOME="$HOME/.local/state"
+
+# $XDG_RUNTIME_DIR should be set by the system. Its access mode MUST be 0700
 
 # Unofficial XDG, extended specifications
 # These are not standardized but are useful and common
