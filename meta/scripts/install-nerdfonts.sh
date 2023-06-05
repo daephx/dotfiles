a29b716453f3dc02a94ed0707d3f9bdcc438cd82
daephx <daephxdev@gmail.com>
2023-06-05 06:11:31 -0700

    feat: remove fonts and add nerdfont install script

---
+++ meta/scripts/install-nerdfonts.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+FONTS="${XDG_DATA_HOME:-$HOME/.local/share}/fonts"
+
+echo "download nerdfonts..."
+
+pushd "$(mktemp -d)" || exit
+
+for font in "${@:-NerdFontsSymbolsOnly}"; do
+  url="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${font}.tar.xz"
+  archive=$(basename "$url")
+  dir=$($archive | cut -d. -f1)
+  echo "$url"
+  curl -LO "$url"
+  [ -d "$dir" ] && rm -rf "$dir"
+  tar xf "$archive" --one-top-level -C "$FONTS"
+done
+
+popd || exit
+
+echo "rebuilding font-cache..."
+fc-cache -fv
+echo "done!"
