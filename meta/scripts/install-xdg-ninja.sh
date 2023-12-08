c664b9fdf92c732d76cec51ef2e34579666b0ffe
daephx <daephxdev@gmail.com>
2023-12-07 16:56:52 -0800

    feat: add install/update script for xdg-ninja

---
+++ meta/scripts/install-xdg-ninja.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO_URL="https://github.com/b3nj5m1n/xdg-ninja.git"
+INSTALL_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/xdg-ninja"
+BIN_DIR="${XDG_LOCAL_HOME:-$HOME/.local/}/bin"
+
+# Clone latest repository version
+if [ ! -d "$INSTALL_DIR" ]; then
+  git clone --depth 1 $REPO_URL "$INSTALL_DIR"
+else
+  branch=$(git -C "$INSTALL_DIR" rev-parse --abbrev-ref origin/HEAD | cut -c8-)
+  echo "Repository already exists: Updating origin/$branch"
+  git -C "$INSTALL_DIR" pull origin "$branch"
+fi
+
+# Create symbolic link to script
+ln -sfn "$INSTALL_PATH/xdg-ninja.sh" "$BIN_DIR/xdg-ninja"
