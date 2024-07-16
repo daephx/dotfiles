63aa0584c770c025ea40fa1ad0507e66bb202940
daephx <daephxdev@gmail.com>
2024-07-16 01:28:11 -0700

    feat(zsh): exclude system users from completion
    For common tasks, listing all of the system users just adds noise to the
    completion results. By adding a number of ignore patterns, we can
    simplify the user list to only show usernames we are interested in.
    
    There my be cases where we want to modify one or more of these users but
    I remain uncertain as to how common this is in practice. So for now,
    we can keep this since the exact snippet is also present within the
    oh-my-zsh base configuration.

--- config/zsh/completion.zsh
+++ config/zsh/completion.zsh
@@ -74,6 +74,17 @@ zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*:messages' format $'\e[01;35m[%d]\e[0m'
 zstyle ':completion:*:warnings' format $'\e[01;31m[No Matches Found]\e[0m'
 
+# Exclude system users from completion
+zstyle ':completion:*:*:*:users' ignored-patterns \
+  adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
+  clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
+  gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
+  ldap lp mail mailman mailnull man messagebus  mldonkey mysql nagios \
+  named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
+  operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
+  rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
+  usbmux uucp vcsa wwwrun xfs '_*'
+
 # Completion options for kill/processes
 compdef pkill=kill
 compdef pkill=killall
