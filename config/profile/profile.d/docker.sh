dfa05425ebbd43045bc1362a06771b9e724406ad
daephx <daephxdev@gmail.com>
2024-07-26 05:33:22 -0700

    feat(profile): add Docker configuration and utility aliases
    - Set Docker configuration directory using XDG Base Directory specification
    - Add aliases for Docker operations:
      - View running containers with scrolling
      - Pull images and list running containers
      - Run the container top utility

---
+++ config/profile/profile.d/docker.sh
@@ -0,0 +1,16 @@
+# docker: configuration and utility settings
+# shellcheck shell=sh
+
+# Set the Docker configuration directory using XDG Base directory specification.
+export DOCKER_CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/docker"
+
+# Aliases for Docker operations:
+
+# View running containers with scrolling
+alias dps="docker ps | less -S"
+
+# Pull images and list running containers
+alias dcup="docker compose pull && docker compose ps --format '{{.Names}}'"
+
+# Run the container top utility from within docker
+alias ctop='docker run -it --rm --name=ctop -v /var/run/docker.sock:/var/run/docker.sock quay.io/vektorlab/ctop:latest'
