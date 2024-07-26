4add7eaea2bca3fc5d65656bb209e02d63cf72f3
daephx <daephxdev@gmail.com>
2024-07-26 04:39:57 -0700

    docs(profile): update comments for XDG base directory setup
    Refactor comments to improve clarity and consistency regarding the XDG
    Base Directory Specification and related environment variables.

--- config/profile/profile.d/10-xdg-base-dirs.sh
+++ config/profile/profile.d/10-xdg-base-dirs.sh
@@ -1,15 +1,16 @@
+# XDG: Follow the XDG Base Directory Specification
 # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 
-# XDG base directory specifications
+# NOTE: $XDG_RUNTIME_DIR should be set by the system.
+# Its access mode MUST be 0700 for security purposes.
+
+# Standard XDG base directories
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_LOCAL_HOME="$HOME/.local"
 export XDG_STATE_HOME="$HOME/.local/state"
 
-# $XDG_RUNTIME_DIR should be set by the system. Its access mode MUST be 0700
-
-# Unofficial XDG, extended specifications
-# These are not standardized but are useful and common
+# Unofficial, extended XDG directories (not standardized but commonly used)
 export XDG_BIN_HOME="$HOME/.local/bin"
 export XDG_LIB_HOME="$HOME/.local/lib"
