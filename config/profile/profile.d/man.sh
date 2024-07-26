9bf08dc89eabebc8aa171e47c7b7be3e044cb4b1
daephx <daephxdev@gmail.com>
2024-07-26 01:45:07 -0700

    docs(profile): update man page settings and comments
    - Added comments to explain variables.
    - Removed custom MANPAGER variable; using default settings.
    - Added export for MANWIDTH to set maximum width for man page content.

--- config/profile/profile.d/man.sh
+++ config/profile/profile.d/man.sh
@@ -1,4 +1,9 @@
-# Man pager
-# Define how to display man pages
+# man: an interface to the system reference manuals
+# shellcheck shell=sh
+
+# Define how to display man pages.
 # export MANPAGER="less -sMJ +Gg"
-export MANPAGER="sh -c 'col -bx | bat -l man -p'" # batcat
+# export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
+# Set maximum width for manpage content.
+export MANWIDTH=120
