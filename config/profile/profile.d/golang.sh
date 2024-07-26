9e0297028750c35ad0c682277fa25924905093de
daephx <daephxdev@gmail.com>
2024-07-25 17:56:15 -0700

    fix(profile): update Go environment and comments
    - Added fallback for `GOPATH` if `XDG_LOCAL_HOME` is not set.
    - Refined PATH handling to prevent duplicate entries for Go binaries.
    - Updated comments for clarity and consistency.

--- config/profile/profile.d/golang.sh
+++ config/profile/profile.d/golang.sh
@@ -1,9 +1,13 @@
-# Go settings
-export GOPATH="$XDG_LOCAL_HOME/lib/go"
+# Go: Managing Golang environment
 
+# Set preferred directory for GOPATH
+export GOPATH="${XDG_LOCAL_HOME:-$HOME/.local}/lib/go"
+
+# Add go directories to PATH
 if ! echo "$PATH" | grep -Eq "(^|:)/usr/local/go/bin($|:)"; then
-  export PATH="$PATH:/usr/local/go/bin"
+  PATH="${PATH:+${PATH}:}/usr/local/go/bin"
 fi
+
 if ! echo "$PATH" | grep -Eq "(^|:)$GOPATH/bin($|:)"; then
-  export PATH="$PATH:$GOPATH/bin"
+  PATH="${PATH:+${PATH}:}$GOPATH/bin"
 fi
