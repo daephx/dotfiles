a95e1a4aa735818dafbd228271a8479d0a91cacb
daephx <daephxdev@gmail.com>
2024-07-26 01:37:35 -0700

    docs(profile): update rust environment and comments
    - Improved header comment for Rust toolchain configuration
    - Added details for custom directories and environment loading
    - Changed from `source` to `.` for POSIX compliance

--- config/profile/profile.d/rust.sh
+++ config/profile/profile.d/rust.sh
@@ -1,6 +1,12 @@
-# Rust settings
+# Rust: Configuration for Rust toolchain
+# shellcheck disable=SC1091
+# shellcheck shell=sh
+
+# Define custom directories for Rust tools.
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
+
+# Load Cargo environment if the file exists.
 if [ -f "$CARGO_HOME/env" ]; then
-  source "$CARGO_HOME/env"
+  . "$CARGO_HOME/env"
 fi
