72fcbec78c64096a15e741029ba2f83773cfdd84
daephx <daephxdev@gmail.com>
2024-08-08 16:31:50 -0700

    feat(profile): update Lua and Luarocks path configuration
    - Adjust custom Lua library path to use fallback XDG directories.
    - Unset temporary LUA_LIB variable after use.
    - Set custom configuration path for Luarocks.
    - Add fallback path to XDG directories.

--- config/profile/profile.d/lua.sh
+++ config/profile/profile.d/lua.sh
@@ -1,7 +1,11 @@
-# Lua Settings
-# Trailing ;; ensure that default path will be appended by Lua
-LUA_LIB="$XDG_LOCAL_HOME/lib/lua" # Custom module location
+# Lua Settings: Configure Lua and Luarocks paths.
+
+# Add custom Lua library path to the Lua search paths.
+# The trailing ;; ensures that the default paths are also included by Lua.
+LUA_LIB="${XDG_LOCAL_HOME:-$HOME/.local}/lib/lua" # Custom module location
 export LUA_PATH="$LUA_PATH;$LUA_LIB/?.lua;$LUA_LIB/?/init.lua;;"
 export LUA_CPATH="$LUA_CPATH;$LUA_LIB/?.so;$LUA_LIB/?/init.so;;"
-# export LUAROCKS_CONFIG="$XDG_CONFIG_HOME/luarocks/config*.lua"
-unset LUA_LIB
+unset LUA_LIB # Unset temporary variable after use.
+
+# Set the custom configuration path for Luarocks.
+export LUAROCKS_CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/luarocks/config*.lua"
