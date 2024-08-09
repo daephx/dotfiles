7e6a03f4cbe1ad8feb424ad29366796c4b529965
daephx <daephxdev@gmail.com>
2024-08-08 17:10:31 -0700

    refactor(profile): move functions and clean up aliases script
    - Remove `functions.sh` and move custom functions into `aliases.sh`.
    - Clean up `aliases.sh` by organizing aliases into sections.
    - Remove unused and redundant aliases.
    - Update comments and structure for better readability.

--- config/profile/profile.d/aliases.sh
+++ config/profile/profile.d/aliases.sh
@@ -1,5 +1,7 @@
-# ~/.aliases: user aliases for interactive POSIX shells
+# aliases: user aliases for interactive POSIX shells
 # shellcheck disable=1001
+# shellcheck disable=2148
+# shellcheck shell=bash
 
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
@@ -14,40 +16,40 @@ alias ...="cd ../.."         # 2 Parents
 alias ..="cd .."             # 1 Parent
 alias ~="cd ~"               # HOME `cd`|`~`
 
-alias tmp="cd /tmp"  # System Temp
 alias temp="cd /tmp" # System Temp
+alias tmp="cd /tmp"  # System Temp
 
-# Common mistakes
-alias brwe="brew"
-alias where="which" # Damn it Microsoft!
-alias emasc="emacs"
-alias emcas="emacs"
-
-# Exit mistakes
-alias \:q='exit'
-alias \:qa='exit'
-alias \:xa="exit"
-alias eexit="exit"
-alias eit="exit"
-alias exot="exit"
-alias Q='exit'
-alias q='exit'
-alias qa='exit'
-alias x='exit'
-alias Xa="exit"
-alias xa="exit"
-alias xit="exit"
-
-# Editors
-alias e="eval \$VISUAL"
-alias edit="eval \$VISUAL"
-
-# TODO: vi should be vim/nvim: never emacs
-# Instead of setting to visual, scrape for nvim, vim, vi in that order
-alias vi="eval \$VISUAL"
+# Add options to common file operations
+alias cp="cp -riv"
+alias mv="mv -iv"
+alias rm='rm -iv'
 
 # Clear terminal output
 alias cls="clear"
+
+# Hide exit output
+alias exit="exit &>/dev/null"
+
+# Quick exit
+alias q='exit'
+
+# Make directory: verbose, parent
+alias mkdir="mkdir -vp"
+
+# Enable sudo to expand aliases
+alias sudo="sudo"
+
+# shorten xdg_open
+alias open='xdg-open &>/dev/null'
+
+# Shorthand to display command history
+alias hist="history"
+
+# Output the current shell PID
+alias shell="ps -p \$$"
+
+# Print each PATH entry on a separate line
+alias path='echo -e "${PATH//:/\\n}" | less'
 
 # list mounted drives
 alias mnt="mount | grep -E ^/dev | column -t | less"
@@ -55,62 +57,36 @@ alias mnt="mount | grep -E ^/dev | column -t | less"
 # Count number of files in current directory
 alias count='find . -type f | wc -l'
 
-# Output the current shell PID
-alias shell="ps -p \$$"
-
-# shorten xdg_open
-alias open='xdg-open &>/dev/null'
-
-# Hide exit output
-alias exit="exit &>/dev/null"
-
-# Enable sudo to expand aliases
-alias sudo="sudo"
-
-# Reload current shell session
-reload() {
-  clear && exec $(sh -c 'ps -p $$ -o ppid=' \
-    | xargs -I'{}' readlink -f '/proc/{}/exe')
-}
-
-# Print each PATH entry on a separate line
-alias path='echo -e "${PATH//:/\\n}" | less'
-
-# Shorthand to display command history
-alias hist="history"
-
 # Recursively delete empty directories
 alias empty="find . -empty -type d -print -delete"
 
-# Prefer less as pager
-alias more="less"
-
-# Go to sleep
-if command -v systemctl > /dev/null; then
-  alias zzz="systemctl suspend"
-fi
+# Common mistakes
+alias brwe="brew"
+alias emasc="emacs"
+alias emcas="emacs"
+alias where="which"
 
 ### Utilities ###
+
+# Send files to Trash from command line
+# alias trash="mv --force -t ~/.local/share/Trash"
+# alias rm='trash -v'
+
+# Copy alternative with progress bar
+alias cpv="rsync -ah --info=progress2"
 
 # Always enable colored `grep` output
 alias grep="grep --color=auto"
 alias egrep="egrep --color=auto"
 alias fgrep="fgrep --color=auto"
 
-# File access
-alias zconf="vi \$XDG_CONFIG_HOME/zsh/zshrc"
-alias bconf="vi \$XDG_CONFIG_HOME/bash/bashrc"
-alias cp="cp -riv"
-alias mv="mv -iv"
-alias mkdir="mkdir -vp"
+# File size
+alias fs="stat -f \"%z bytes\""
 
 # human-readable sizes
 alias df="df -h"
 
-# File size
-alias fs="stat -f \"%z bytes\""
-
-# un tar archive
+# extract tar archive
 alias untar='tar xvf'
 
 # Recursively delete `.DS_Store` files
@@ -119,77 +95,46 @@ alias ds_cleanup="find . -type f -name '*.DS_Store' -ls -delete"
 # Return the current public IP address
 alias myip="curl http://ipecho.net/plain; echo"
 
+# Get week number
+alias week='date +%V'
+
+# Prefer less as pager
+if ! command -v more > /dev/null; then
+  alias more="less"
+fi
+
 # forgetful ifconfig
 if ! command -v ifconfig > /dev/null; then
   alias ifconfig='ip -c a'
 fi
 
-# Git version control
-if command -v git > /dev/null; then
-  alias difftool="git difftool"
-  alias g="git"
-  alias gb="git branch"
-  alias gc="git commit"
-  alias gs="git show"
-  alias gs="git status"
-  alias gti="git"
-fi
+# Copy commands: xclip
+if command -v xclip > /dev/null; then
+  # copy working directory
+  alias cwd='pwd | tr -d "\r\n" | xclip -selection clipboard && echo "=> Working Directory copied to clipboard."'
 
-gr() {
-  local root
-  root="$(git rev-parse --show-toplevel)"
-  cd "$root" && echo "$root"
-}
+  # Pipe my public key to my clipboard.
+  alias public_key="less ~/.ssh/id_ed25519.pub | xclip -selection clipboard && echo '=> Public key copied to clipboard.'"
 
-gwt() {
-  local wtdir query
-  query="${1:- }"
-  wtdir=$(
-    git worktree list \
-      | fzf --preview-window up,5,border-horizontal \
-        --preview='git log --color=always --oneline --graph -n10 {2}' \
-        --query "$query" -1 \
-      | awk '{print $1}'
-  )
-  [ -n "$wtdir" ] && cd "$wtdir" || return
-}
-
-_fzf_comprun() {
-  local command=$1
-  shift
-
-  case "$command" in
-    cd) fzf "$@" --preview 'tree -C {} | head -200' ;;
-    *) fzf "$@" ;;
-  esac
-}
-
-# Create gitignore file from online template
-_git_new_ignore() {
-  local url
-  url="https://www.toptal.com/developers/gitignore/api"
-  curl -sL "$url/$*" >> .gitignore
-}
-alias ignore="_git_new_ignore"
-
-# Docker container list
-if command -v docker > /dev/null; then
-  alias dps="docker ps | less -S"
-  alias ctop='docker run --rm -ti --name=ctop -v /var/run/docker.sock:/var/run/docker.sock quay.io/vektorlab/ctop:latest'
-  alias dcup="docker compose pull && docker compose ps --format '{{.Names}}'"
+  # Pipe my private key to my clipboard.
+  alias private_key="less ~/.ssh//id_rsa | xclip -selection clipboard && echo '=> Private key copied to clipboard.'"
 fi
 
 # bat: Replacement command for cat
 if command -v bat > /dev/null; then
   _batlog() {
-    tail -f "$1" | bat -l=log --theme base16 --pager=never --color=always --decorations=never
-  }
-  _bathelp() {
-    "$@" --help 2>&1 | bat --plain --language=help
+    tail -f "$1" | bat -l=log --pager=never --color=always --decorations=never
   }
   alias cat="bat"
   alias help='_bathelp'
   alias logf="_batlog"
+fi
+
+# Ripgrep paging
+# alias rg='rg -p -i'
+# NOTE: Json flag doesn't work with other flags like files-with-matches
+if command -v delta > /dev/null; then
+  rg() { command rg --json "$@" | delta; }
 fi
 
 # Ensure NCDU utility starts with colors
@@ -198,67 +143,65 @@ if command -v ncdu > /dev/null; then
   alias du='ncdu --color dark'
 fi
 
-# Xclip commands
-if command -v xclip > /dev/null; then
-  # copy working directory | Requires xclip
-  alias cwd='pwd | tr -d "\r\n" | xclip -selection clipboard && echo "=> Working Directory copied to pasteboard."'
-
-  # Pipe my public key to my clipboard. | Requires xclip
-  alias public_key="less ~/.ssh/id_ed25519.pub | xclip -selection clipboard && echo '=> Public key copied to pasteboard.'"
-
-  # Pipe my private key to my clipboard. | Requires xclip
-  alias private_key="less ~/.ssh//id_rsa | xclip -selection clipboard && echo '=> Private key copied to pasteboard.'"
-fi
-
 # dstask: shorthand to 'task'
 if command -v dstask > /dev/null; then
   alias task="dstask"
 fi
 
-# TODO: Include fzf command references from wiki, specifically docker.
-# https://github.com/junegunn/fzf/wiki/examples#man-pages
-
-fman() {
-  man -k . \
-    | fzf -q "$1" \
-      --height=100% \
-      --prompt='man> ' \
-      --preview $'echo {} | tr -d \'()\' | awk \'{printf "%s ", $2} {print $1}\' | xargs -r man | col -bx | bat -l man -p --color always' | tr -d '()' | awk '{printf "%s ", $2} {print $1}' | xargs -r man
+# Reload current shell session
+reload() {
+  clear && exec $(sh -c 'ps -p $$ -o ppid=' \
+    | xargs -I'{}' readlink -f '/proc/{}/exe')
 }
-# Get the colors in the opened man page itself
-# export MANPAGER="sh -c 'col -bx | bat -l man -p --paging always'"
 
-if command -v fzf > /dev/null; then
-  __fzf_widget_man() {
-    if [ -n "$1" ]; then command man "$1" && return 0; fi
-    # res=$(command man -k . | fzf --preview="command man $(echo "{}" | cut -d ' ' -f1)")
-    res=$(command man -k . | fzf --preview="cmd=\$(echo '{}' | cut -d ' ' -f1); command man \$cmd")
-    if [ -n "$res" ]; then
-      clear # Prevent prompt display error
-      command man "$(echo "$res" | cut -d ' ' -f1)"
-    fi
-  }
+# Create and change directory
+# Usage: mk </dir/path>
+mk() { mkdir -p -- "$1" && cd -P -- "$1" || return; }
 
-  alias \?="__fzf_widget_man"
-  # alias man="__fzf_widget_man"
-fi
+# Extract archive formats
+# Usage: extract <path/to/file>
+extract() {
+  if [ -f "$1" ]; then
+    case "$1" in
+      *.tar.bz2) tar xjf "$1" ;;
+      *.tar.gz) tar xzf "$1" ;;
+      *.tar.xz) tar xf "$1" ;;
+      *.tar.zst) unzstd "$1" ;;
+      *.bz2) bunzip2 "$1" ;;
+      *.rar) rar x "$1" ;;
+      *.gz) gunzip "$1" ;;
+      *.tar) tar xf "$1" ;;
+      *.tbz2) tar xjf "$1" ;;
+      *.tgz) tar xzf "$1" ;;
+      *.zip) unzip "$1" -d "${1%.zip}" ;;
+      *.Z) uncompress "$1" ;;
+      *.7z) 7z x "$1" ;;
+      *.deb) ar x "$1" ;;
+      *) printf "\e[31mE\e[0m: Unsupported filetype: '%s'\n" "$1" ;;
+    esac
+  else
+    printf "\e[31mE\e[0m: Invalid file: '%s'\n" "$1"
+  fi
+}
 
-# cd and list directory
-cx() { cd "$@" && ls -l; }
+# Get absolute path
+# Usage: abspath <path/to/item>
+abspath() {
+  cd "$(dirname "$1")" > /dev/null || return
+  printf "%s/%s\n" "$(pwd)" "$(basename "$1")"
+  cd - || return
+}
 
-# Ripgrep paging
-# alias rg='rg -p -i'
-# NOTE: Json flag doesn't work with other flags like files-with-matches
-# if command -v delta >/dev/null; then
-#   rg() { command rg --json "$@" | delta; }
-# fi
+# Rename current working directory
+rename() {
+  cd . || return
+  new_dir=${PWD%/*}/$1
+  mv -- "$PWD" "$new_dir" \
+    && cd -- "$new_dir" || exit
+}
 
-# Send files to Trash from command line
-alias trash="mv --force -t ~/.local/share/Trash"
-
-# Copy alternative with progress bar
-alias cpv="rsync -ah --info=progress2"
-
-# Python create virtual environment
-alias ve="python -m venv ./.venv"
-alias va="source ./venv/bin/activate"
+# Create path on touch
+# Usage: touchd <path/to/file>
+touchd() {
+  mkdir -p "${1%/*}" && touch "$1"
+}
