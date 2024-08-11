a964a107908c48e4d177648d60aa33be2010f0e7
daephx <daephxdev@gmail.com>
2024-08-10 22:14:03 -0700

    refactor(profile): move dotnet config path to `XDG_DATA_HOME/dotnet`

--- config/profile/profile.d/dotnet.sh
+++ config/profile/profile.d/dotnet.sh
@@ -4,6 +4,9 @@
 # Disable telemetry for .NET CLI
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
+# Set the configuration directory for dotnet-cli
+export DOTNET_CLI_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"/dotnet
+
 # Set the configuration directory for OmniSharp
 export OMNISHARPHOME="${XDG_CONFIG_HOME:-$HOME/.config}/omnisharp"
 
