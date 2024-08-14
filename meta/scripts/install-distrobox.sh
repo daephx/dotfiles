9017fa779b60a7ae691e8204164e7aa498330243
daephx <daephxdev@gmail.com>
2024-08-14 03:19:31 -0700

    refactor(xdg)!: relocate locally installed programs to `.local/src`
    Introduces a non-standard XDG directory variable `XDG_SRC_HOME`, which
    mirrors `/usr/local/src` for the user's home directory. This provides a
    designated location for managing and compiling projects manually
    installed from source.

--- meta/scripts/install-distrobox.sh
+++ meta/scripts/install-distrobox.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 # Installer script for distrobox
 REPO_URL="https://github.com/89luca89/distrobox.git"
-INSTALL_DIR="/tmp/distrobox"
-LATEST=$(git ls-remote --tags --refs $REPO_URL | tail -n1 | cut -d/ -f3)
+INSTALL_DIR="${XDG_SRC_DIR:-$HOME/.local/src}/distrobox"
+LATEST_RELEASE=$(git ls-remote --tags --refs $REPO_URL | tail -n1 | cut -d/ -f3)
 
 echo "Cloning repository: $REPO_URL"
 echo ""
-echo "  Release tag:  $LATEST"
+echo "  Release tag:  $LATEST_RELEASE"
 echo "  Install path: $INSTALL_DIR"
 echo ""
 
 # Clone latest repository version
 if [ ! -d "$INSTALL_DIR" ]; then
   echo "Cloning repository..."
-  git clone --depth 1 $REPO_URL --single-branch --branch "$LATEST" \
+  git clone --depth 1 $REPO_URL --single-branch --branch "$LATEST_RELEASE" \
     -c advice.detachedHead=false "$INSTALL_DIR"
 else
   HEAD=$(git -C "$INSTALL_DIR" rev-list --tags --max-count=1)
   CURRENT=$(git -C "$INSTALL_DIR" describe --tags "$HEAD")
-  if [ "$CURRENT" = "$LATEST" ]; then
+  if [ "$CURRENT" = "$LATEST_RELEASE" ]; then
     echo "Repository is already up-to-date!"
     exit 0
   else
     echo "Updating repository..."
-    git -C "$INSTALL_DIR" fetch --depth 1 origin tag "$LATEST"
-    git -C "$INSTALL_DIR" switch --detach "$LATEST"
+    git -C "$INSTALL_DIR" fetch --depth 1 origin tag "$LATEST_RELEASE"
+    git -C "$INSTALL_DIR" switch --detach "$LATEST_RELEASE"
   fi
 fi
 
