33aebd9868628ae52474d71a4d68790d13d343f0
daephx <daephxdev@gmail.com>
2024-08-23 16:30:36 -0700

    fix(profile): remove unused exports `PYTHON_USER_LIB` and `PYTHONPATH`
    Addressed an issue where `PYTHONPATH` was accumulating duplicate entries
    due to re-sourcing. Since the custom path configuration is not being
    used, it's more effective to remove these exports altogether instead of
    resolving the duplication.

--- config/profile/profile.d/python.sh
+++ config/profile/profile.d/python.sh
@@ -4,13 +4,6 @@
 # Exit if neither Python 2 nor Python 3 is installed
 [ ! -x "$(command -v python)" ] && [ ! -x "$(command -v python3)" ] && return
 
-# Define user-specific Python library path and create it if it doesn't exist
-PYTHON_USER_LIB=${XDG_DATA_HOME:-$HOME/.local/share}/python/lib
-[ ! -d "$PYTHON_USER_LIB" ] && mkdir -p "$PYTHON_USER_LIB" &> /dev/null
-
-# Add user library to PYTHONPATH for custom modules and packages
-export PYTHONPATH="$PYTHONPATH:$PYTHON_USER_LIB"
-
 # Set Python startup file for REPL customization
 export PYTHONSTARTUP="${XDG_CONFIG_HOME:-$HOME/.config}/python/pythonrc.py"
 
