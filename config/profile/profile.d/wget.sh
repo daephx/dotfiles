449bdb47c2127835e0adffb199904ea88d626790
daephx <daephxdev@gmail.com>
2025-06-25 17:28:33 -0700

    fix: handle missing wgetrc to prevent wget errors
    Unset the WGETRC environment variable if the wgetrc file does not
    exist. This prevents wget from raising errors due to a missing
    configuration file, which could cause any script or command using
    wget to fail.
    
    Also ensure the parent directory for wget history is created before
    use.

--- config/profile/profile.d/wget.sh
+++ config/profile/profile.d/wget.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env sh
 # WGet: Set custom configuration and history file.
 
-# Set Wget configuration and history paths.
-export WGETRC="$XDG_CONFIG_HOME/wget/wgetrc"
-export WGETHIST="$XDG_CACHE_HOME/wget/history"
+# Set Wget configuration variables.
+export WGETHIST="${XDG_STATE_HOME:-$HOME/.local/share}/wget/history"
+export WGETRC="${XDG_CONFIG_HOME:-$HOME/.config}/wget/wgetrc"
+
+# Create parent directory for wget history file if not exists.
+wget_data_home="$(dirname $WGETHIST)"
+[ ! -d "$wget_data_home" ] && mkdir -p "$wget_data_home" >/dev/null
+
+# Unset the environment variable if the file doesn't exist.
+# This prevents file not found errors from occuring for commands that use wget.
+[ ! -f "$WGETRC" ] && unset WGETRC
 
 # Alias Wget to use custom history file as Wget doesn't support it via env vars.
 alias wget="wget --hsts-file \$WGETHIST"
